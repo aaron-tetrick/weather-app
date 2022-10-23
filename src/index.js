2d7877e8b6f893bd3b9b3d5a807b468caafc0deb
Aaron Tetrick <atetrick14102@gmail.com>
2022-10-23 04:19:11 -0400

    Add functionality and logic to the 'farenheit/celcius' button to change values on display

--- src/index.js
+++ src/index.js
@@ -6,30 +6,65 @@ class WX {
         const location =  document.getElementById('location');
         const weather = document.getElementById('weather');
         const feel = document.getElementById('feel');
-        const humidity =  document.getElementById('humidity')
+        const humidity =  document.getElementById('humidity');
         const pressure = document.getElementById('pressure');
         const high =  document.getElementById('high');
         const low = document.getElementById('low');
-        
+        const far = document.getElementById('far');
+        const cel = document.getElementById('cel');
+
         if (city !== '') {
         fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=fd750b4ded1cc447604504ec4b1324b7`, {mode: 'cors'})
         .then(res => {
             return res.json();
         })
         .then(res => {
-            temp.textContent = (((res.main.temp-273.15)*1.8)+32).toFixed(1) + String.fromCharCode(176);
-            location.textContent = `${res.name}, ${res.sys.country}`
+            location.textContent = `${res.name}, ${res.sys.country}`;
             weather.textContent =  UI.capitalize(res.weather[0].description);
-            feel.textContent = (((res.main.feels_like-273.15)*1.8)+32).toFixed(1) + String.fromCharCode(176);
-            humidity.textContent = res.main.humidity + '%'
+            humidity.textContent = res.main.humidity + '%';
             pressure.textContent = res.main.pressure + 'mb'
-            high.textContent = (((res.main.temp_max-273.15)*1.8)+32).toFixed(1) + String.fromCharCode(176);
-            low.textContent = (((res.main.temp_min-273.15)*1.8)+32).toFixed(1) + String.fromCharCode(176);
+            if(far.className.includes('selected')) {
+                temp.textContent = (((res.main.temp-273.15)*1.8)+32).toFixed(1) + String.fromCharCode(176);
+                feel.textContent = (((res.main.feels_like-273.15)*1.8)+32).toFixed(1) + String.fromCharCode(176);
+                high.textContent = (((res.main.temp_max-273.15)*1.8)+32).toFixed(1) + String.fromCharCode(176);
+                low.textContent = (((res.main.temp_min-273.15)*1.8)+32).toFixed(1) + String.fromCharCode(176);
+            } else if (cel.className.includes('selected')) {
+                temp.textContent = (res.main.temp-273.15).toFixed(1) + String.fromCharCode(176);
+                feel.textContent = (res.main.feels_like-273.15).toFixed(1) + String.fromCharCode(176);
+                high.textContent = (res.main.temp_max-273.15).toFixed(1) + String.fromCharCode(176);
+                low.textContent = (res.main.temp_min-273.15).toFixed(1) + String.fromCharCode(176);
+            }
         })
         } else { 
         console.log("NOTHING ENTERED")
-        }
-    } 
+        };
+    } ;
+
+    static selectTemp() {
+        let temp = document.getElementById('temp');
+        let feel = document.getElementById('feel');
+        let high =  document.getElementById('high');
+        let low = document.getElementById('low');
+        let far = document.getElementById('far');
+        let cel = document.getElementById('cel');
+        let newTemp = temp.textContent.slice(0, -1);
+        let newFeel = feel.textContent.slice(0, -1);
+        let newHigh = low.textContent.slice(0, -1);
+        let newLow = temp.textContent.slice(0, -1);
+        if(!temp.textContent.includes('--')) {
+            if(cel.className.includes('selected')) {
+                temp.textContent = ((newTemp-32)/1.8).toFixed(1) + String.fromCharCode(176);
+                feel.textContent = ((newFeel-32)/1.8).toFixed(1) + String.fromCharCode(176);
+                high.textContent = ((newHigh-32)/1.8).toFixed(1) + String.fromCharCode(176);
+                low.textContent = ((newLow-32)/1.8).toFixed(1) + String.fromCharCode(176);
+            } else if(far.className.includes('selected')) {
+                temp.textContent = ((newTemp*1.8)+32).toFixed(1) + String.fromCharCode(176);
+                feel.textContent = ((newFeel*1.8)+32).toFixed(1) + String.fromCharCode(176);
+                high.textContent = ((newHigh*1.8)+32).toFixed(1) + String.fromCharCode(176);
+                low.textContent = ((newLow*1.8)+32).toFixed(1) + String.fromCharCode(176);
+            };
+        };
+    };
 };
 
 class UI {
@@ -41,9 +76,25 @@ class UI {
             weather.style.fontSize = '4rem';
         }
         return desc.charAt(0).toUpperCase() + desc.slice(1);
-    }
-}
+    };
+
+    static boldText() {
+        const far = document.getElementById('far');
+        const cel = document.getElementById('cel');
+        console.log(far.classList, cel);
+        if(far.className.includes('selected')) {
+            far.classList.remove('selected');
+            cel.classList.add('selected');
+        } else if (cel.className.includes('selected')) {
+            cel.classList.remove('selected');
+            far.classList.add('selected');
+        };
+    };
+};
 
 document.querySelector('.search-btn').addEventListener('click', WX.getInfo);
 
-
+document.querySelector('.temp-btn').addEventListener('click',  () => {
+    UI.boldText();
+    WX.selectTemp();
+});
